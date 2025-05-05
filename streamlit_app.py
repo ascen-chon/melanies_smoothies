3be794935934f1a6847ce808fd893be1fbfa0bd5
ascen-chon <ascen@posteo.net>
2025-05-05 16:45:48 +1000

    Update streamlit_app.py
    json reply

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
     #New section to reqeuest smoothiefroot information
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response)
+    st.text(smoothiefroot_response.json())
     #st.write(my_insert_stmt)
     #st.stop()
     time_to_insert = st.button ('Submit Order')
