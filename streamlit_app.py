fba158279b154269f2bde5ee3a6966e74c38455f
ascen-chon <ascen@posteo.net>
2025-05-05 17:23:28 +1000

    Update streamlit_app.py
    Add Datastream and json

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,7 @@ if ingredients_list:
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(my_insert_stmt)
     #st.stop()
     time_to_insert = st.button ('Submit Order')
